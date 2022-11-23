eba56603d939623876ef4fab9e65d85201341bb8
srSenhor <viniciusfeliciano14@gmail.com>
2022-11-23 15:55:17 -0300

    Testando algumas mudanças

--- src/br/edu/ufersa/wsgear/view/Telas.java
+++ src/br/edu/ufersa/wsgear/view/Telas.java
@@ -17,7 +17,7 @@ public class Telas extends Application{
 	public void start(Stage arg0) throws Exception {
 		setStage(arg0);
 		arg0.setTitle("Teste");
-		telaCadastroAluno();
+		testeTela();
 	}
 	
 	
@@ -27,29 +27,18 @@ public class Telas extends Application{
 	
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// exemplo gadelha																						//
-	public static void telaCadastroAluno() {																//
+	public static void testeTela() {																//
 		try {																								//
-			Parent root = FXMLLoader.load(Telas.class.getResource("resources/mainScreen.fxml"));			//
+			Parent root = FXMLLoader.load(Telas.class.getResource("view/resources/pesquisaScreen.fxml"));			//
 			Scene scene = new Scene (root);																	//
 			stage.setScene(scene);																			//
-			stage.setTitle("Tela de Cadastro");													//
+			stage.setTitle("Teste");													//
 			stage.show();																					//
 		}																									//
 		catch(Exception e) {																				//
 			e.printStackTrace();																			//
 		}																									//
 	}																										//
-	public static void listarAlunos() {																		//
-		try {																								//
-			Parent root = FXMLLoader.load(Telas.class.getResource("resources/listarAluno.fxml"));			//
-			Scene scene = new Scene (root);																	//
-			stage.setScene(scene);																			//
-			stage.setTitle("Alunos");																		//
-			stage.show();																					//
-		}																									//
-		catch(Exception e) {																				//
-			e.printStackTrace();																			//
-		}																									//
-	}																										//
+																								//
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
