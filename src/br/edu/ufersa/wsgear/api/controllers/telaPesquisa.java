4d0f0484c3c9c76c51ef03e6699aad559d3ecf4f
Brenno Kevyn <99992197+BrennoKM@users.noreply.github.com>
2022-11-23 11:26:14 -0300

    .

---
+++ src/br/edu/ufersa/wsgear/api/controllers/telaPesquisa.java
@@ -0,0 +1,25 @@
+package br.edu.ufersa.wsgear.api.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
+
+public class telaPesquisa {
+	@FXML private TextField pesquisaContent;
+	@FXML private SplitMenuButton pesquisaTipo;
+	@FXML private MenuItem pesAutomovelPlaca;
+	@FXML private MenuItem pesAutomovelCliente;
+	@FXML private MenuItem pesOrcamentoVeiculo;
+	@FXML private MenuItem pesOrcamentoCliente;
+	@FXML private MenuItem pesOrcamentoPeriodo;
+	@FXML private MenuItem pesPecaNome;
+	@FXML private MenuItem pesPecaFabricante;
+	@FXML private MenuItem pesPecaAutomovel;
+	@FXML private MenuItem pesServicoNome;
+	
+	
+	public void pesquisar() {
+		
+	}
+}
