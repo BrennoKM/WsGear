9fb37d8ceaf5351946b4d53d25a5882deb69470c
srSenhor <viniciusfeliciano14@gmail.com>
2022-11-25 18:09:47 -0300

    Adição da tela de 'Concluir Serviço'

---
+++ src/br/edu/ufersa/wsgear/api/controllers/telaConcluirServico.java
@@ -0,0 +1,18 @@
+package br.edu.ufersa.wsgear.api.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class telaConcluirServico {
+	@FXML private TextField valorPagamentoContent;
+	@FXML private TextField idServicoContent;
+	@FXML private Button botaoRealizarPagamento;
+	@FXML private Button homeButton;
+	@FXML private Label mensagemLabel;
+	
+	/*Colocar um método que modifique o valor de mensagemLabel pra notificar se o
+	 *pagamento foi realizado com sucesso ou se nada foi encontrado */
+	 
+}
