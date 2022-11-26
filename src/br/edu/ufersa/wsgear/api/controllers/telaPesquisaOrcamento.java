6310ac51788dc2b198d5493850cffb07f754caf6
srSenhor <viniciusfeliciano14@gmail.com>
2022-11-25 23:29:01 -0300

    Teste

--- src/br/edu/ufersa/wsgear/api/controllers/telaPesquisaOrcamento.java
+++ src/br/edu/ufersa/wsgear/api/controllers/telaPesquisaOrcamento.java
@@ -11,23 +11,36 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class telaPesquisaOrcamento implements Initializable{
-	@FXML private TableView<OrcamentoDTO> tabelaOrcamento;
-	@FXML private TableColumn<OrcamentoDTO, String> idPecaColumn;
-	@FXML private TableColumn<OrcamentoDTO, String> idServicoColumn;
-	@FXML private TableColumn<OrcamentoDTO, String> idAutomovelColumn;
-	@FXML private TableColumn<OrcamentoDTO, String> PrecoColumn;
-	
+public class telaPesquisaOrcamento implements Initializable {
+	@FXML
+	private TableView<OrcamentoDTO> tabelaOrcamento;
+	@FXML
+	private TableColumn<OrcamentoDTO, String> idPecaColumn;
+	@FXML
+	private TableColumn<OrcamentoDTO, String> idServicoColumn;
+	@FXML
+	private TableColumn<OrcamentoDTO, String> idAutomovelColumn;
+	@FXML
+	private TableColumn<OrcamentoDTO, String> PrecoColumn;
+	@FXML
+	private Button editarButton;
+	@FXML
+	private Button cadastrarButton;
+	@FXML
+	private Button excluirButton;
+
 	private OrcamentoBO bo = new OrcamentoBO();
-    private ObservableList<OrcamentoDTO> listaDeOrcamentos;
-    
+	private ObservableList<OrcamentoDTO> listaDeOrcamentos;
+
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		pesquisar();
+		listarOrcamentos();
 	}
+
 	public void listarOrcamentos() {
 		List<OrcamentoDTO> orcamentos = bo.listar();
 		listaDeOrcamentos = FXCollections.observableArrayList(orcamentos);
@@ -36,13 +49,23 @@ public class telaPesquisaOrcamento implements Initializable{
 		idAutomovelColumn.setCellValueFactory(new PropertyValueFactory<>("orcIdAutomovel"));
 		PrecoColumn.setCellValueFactory(new PropertyValueFactory<>("Valor"));
 		tabelaOrcamento.setItems(listaDeOrcamentos);
-		
+
 	}
-	public void pesquisar() {
-		listarOrcamentos();
-	}
+
 	public void chamarTelaPrincipal() {
 		Telas.telaPrincipal();
 	}
-	
+
+	public void cadastrar() {
+
+	}
+
+	public void excluir() {
+
+	}
+
+	public void editar() {
+
+	}
+
 }
